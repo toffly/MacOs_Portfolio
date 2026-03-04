c35d776a4063e6366f53670cb180ce44a3d8a235
toffly <toffly@outlook.com>
2026-03-04 11:00:36 +0700

    type changes

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -3,9 +3,9 @@ import gsap from "gsap";
 import { useRef } from "react";
 
 const FONT_WEIGHTS = {
-  subtitle: {min:100, max: 400, default: 100},
-  title: {min:400, max: 900, default: 400},
-}
+  subtitle: { min: 100, max: 400, default: 100 },
+  title: { min: 400, max: 900, default: 400 },
+};
 
 const renderText = (text: string, className: string, baseWeight = 400) => {
   return [...text].map((char, i) => (
@@ -13,7 +13,7 @@ const renderText = (text: string, className: string, baseWeight = 400) => {
       key={i}
       className={className}
       style={{
-        fontVariationSettings: `'wght' ${baseWeight}'`,
+        fontVariationSettings: `'wght' ${baseWeight}`,
       }}
     >
       {char === "" ? "\u00A0" : char}
@@ -21,58 +21,80 @@ const renderText = (text: string, className: string, baseWeight = 400) => {
   ));
 };
 
-const setupTextHover = ( container, type) => {
-  if(!container) return;
+const setupTextHover = (
+  container: HTMLElement,
+  type: keyof typeof FONT_WEIGHTS,
+) => {
+  if (!container) return;
 
-  const letters = container.querySelectorAll("span")
-  const {min, max, default: base} = FONT_WEIGHTS[type]
+  const letters = container.querySelectorAll<HTMLSpanElement>("span");
+  const { min, max, default: base } = FONT_WEIGHTS[type];
 
-  const animateLetter = (letter, weight, duration = 0.25) => {
-    return gsap.to(letter, {duration, ease: 'power2.out', fontVariationSettings: `'wght' ${weight}'`})
-  }
+  const animateLetter = (
+    letter: HTMLSpanElement,
+    weight: number,
+    duration = 0.25,
+  ) => {
+    return gsap.to(letter, {
+      duration,
+      ease: "power2.out",
+      fontVariationSettings: `'wght' ${weight}`,
+    });
+  };
 
-  const handleMouseMove = (e) => {
-    const {left} = container.getBoundingClientRect()
-    const mouseX = e.clientX - left
+  const handleMouseMove = (e: MouseEvent) => {
+    const { left } = container.getBoundingClientRect();
+    const mouseX = e.clientX - left;
 
     letters.forEach((letter) => {
-      const {left: l , width: w} = letter.getBoundingClientRect()
-      const distance = Math.abs(mouseX - (l - left + w / 2))
-      const intensity = Math.exp(-(distance **2) / 2000)
+      const { left: l, width: w } = letter.getBoundingClientRect();
+      const distance = Math.abs(mouseX - (l - left + w / 2));
+      const intensity = Math.exp(-(distance ** 2) / 2000);
 
-      animateLetter(letter, min + (max - min) * intensity)
-    })
-  }
-  const handleMouseLeave = () => letters.forEach((letter) => animateLetter(letter, base, 0.3))
+      animateLetter(letter, min + (max - min) * intensity);
+    });
+  };
+  const handleMouseLeave = () => {
+    letters.forEach((letter) => {
+      animateLetter(letter, base, 0.3);
+    });
+  };
 
-
-  container.addEventListener("mousemove", handleMouseMove)
-  container.addEventListener("mouseleave", handleMouseLeave)
+  container.addEventListener("mousemove", handleMouseMove);
+  container.addEventListener("mouseleave", handleMouseLeave);
 
   return () => {
-    container.removeEventListener("mousemove", handleMouseMove)
-    container.removeEventListener("mouseleave", handleMouseLeave)
-  }
-}
+    container.removeEventListener("mousemove", handleMouseMove);
+    container.removeEventListener("mouseleave", handleMouseLeave);
+  };
+};
 
 const Welcome = () => {
-  const titleRef = useRef(null);
-  const subtitleRef = useRef(null);
+  const titleRef = useRef<HTMLHeadingElement | null>(null);
+  const subtitleRef = useRef<HTMLParagraphElement | null>(null);
 
-  useGSAP(()=> {
-    const titleCleanup = setupTextHover(titleRef.current, 'title')
-    const subtitleCleanup = setupTextHover(subtitleRef.current, 'subtitle')
+  useGSAP(() => {
+    const titleCleanup = setupTextHover(titleRef.current, "title");
+    const subtitleCleanup = setupTextHover(subtitleRef.current, "subtitle");
 
     return () => {
-      subtitleCleanup()
-      titleCleanup()
-    }
-  },[])
+      subtitleCleanup();
+      titleCleanup();
+    };
+  }, []);
 
   return (
     <section id="welcome">
-      <p ref={subtitleRef}>{renderText("Hey, I'm Peet! Welcome to my", 'text-3xl font-georama', 100)}</p>
-      <h1 ref={titleRef}>{renderText("Portfolio", "text-9xl italic font-georama")} </h1>
+      <p ref={subtitleRef}>
+        {renderText(
+          "Hey, I'm Peet! Welcome to my",
+          "text-3xl font-georama",
+          100,
+        )}
+      </p>
+      <h1 ref={titleRef}>
+        {renderText("Portfolio", "text-9xl italic font-georama")}{" "}
+      </h1>
 
       <div className="small-screen">
         <p> This Portfolio is designed for desktop/tablet screens only.</p>
