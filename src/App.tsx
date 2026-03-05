bec9ebbf5188ec444f3319131389f56ed0830cee
toffly <toffly@outlook.com>
2026-03-05 13:42:58 +0700

    draggable terminal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import { Draggable } from "gsap/Draggable";
+import gsap from "gsap";
 import { Dock, Navbar, Welcome } from "./components";
+import { Terminal } from "./windows";
+
+gsap.registerPlugin(Draggable)
 
 const App = () => {
   return (
@@ -6,6 +11,8 @@ const App = () => {
       <Navbar />
       <Welcome/>
       <Dock/>
+
+      <Terminal/>
     </main>
   );
 };
