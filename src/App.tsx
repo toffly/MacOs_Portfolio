82d1b93f82bfef4a2b771b7ab784c1d12040c1d6
toffly <toffly@outlook.com>
2026-03-06 10:00:25 +0700

    block to interest

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Draggable } from "gsap/Draggable";
 import gsap from "gsap";
 import { Dock, Navbar, Welcome } from "./components";
-import { Terminal } from "./windows";
+import { Safari, Terminal } from "./windows";
 
 gsap.registerPlugin(Draggable)
 
@@ -13,6 +13,7 @@ const App = () => {
       <Dock/>
 
       <Terminal/>
+      <Safari/>
     </main>
   );
 };
